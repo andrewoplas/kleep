236805b010d583212b4d6e6eaa4010f34b39b030
Andrew Oplas <andrewmoplas@gmail.com>
2020-06-14 01:34:59 +0800

    Initial code base

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-'use strict';
-const path = require('path');
-const {app, BrowserWindow, Menu} = require('electron');
+"use strict";
+const path = require("path");
+const { app, BrowserWindow, Menu } = require("electron");
 /// const {autoUpdater} = require('electron-updater');
-const {is} = require('electron-util');
-const unhandled = require('electron-unhandled');
-const debug = require('electron-debug');
-const contextMenu = require('electron-context-menu');
-const config = require('./config');
-const menu = require('./menu');
+const { is } = require("electron-util");
+const unhandled = require("electron-unhandled");
+const debug = require("electron-debug");
+const contextMenu = require("electron-context-menu");
+const config = require("./config");
+
+const appPath = "/app";
 
 unhandled();
 debug();
 contextMenu();
 
 // Note: Must match `build.appId` in package.json
-app.setAppUserModelId('com.company.AppName');
-
-// Uncomment this before publishing your first version.
-// It's commented out as it throws an error if there are no published versions.
-// if (!is.development) {
-// 	const FOUR_HOURS = 1000 * 60 * 60 * 4;
-// 	setInterval(() => {
-// 		autoUpdater.checkForUpdates();
-// 	}, FOUR_HOURS);
-//
-// 	autoUpdater.checkForUpdates();
-// }
+app.setAppUserModelId("com.company.AppName");
 
 // Prevent window from being garbage collected
 let mainWindow;
@@ -38,17 +28,17 @@ const createMainWindow = async () => {
 		height: 400
 	});
 
-	win.on('ready-to-show', () => {
+	win.on("ready-to-show", () => {
 		win.show();
 	});
 
-	win.on('closed', () => {
+	win.on("closed", () => {
 		// Dereference the window
 		// For multiple windows store them in an array
 		mainWindow = undefined;
 	});
 
-	await win.loadFile(path.join(__dirname, 'index.html'));
+	await win.loadFile(path.join(__dirname, appPath + "/index.html"));
 
 	return win;
 };
@@ -58,7 +48,7 @@ if (!app.requestSingleInstanceLock()) {
 	app.quit();
 }
 
-app.on('second-instance', () => {
+app.on("second-instance", () => {
 	if (mainWindow) {
 		if (mainWindow.isMinimized()) {
 			mainWindow.restore();
@@ -68,13 +58,13 @@ app.on('second-instance', () => {
 	}
 });
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
 	if (!is.macos) {
 		app.quit();
 	}
 });
 
-app.on('activate', async () => {
+app.on("activate", async () => {
 	if (!mainWindow) {
 		mainWindow = await createMainWindow();
 	}
@@ -82,9 +72,5 @@ app.on('activate', async () => {
 
 (async () => {
 	await app.whenReady();
-	Menu.setApplicationMenu(menu);
 	mainWindow = await createMainWindow();
-
-	const favoriteAnimal = config.get('favoriteAnimal');
-	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
 })();
